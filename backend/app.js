832d275d0449b541eedc46db483312de10ed43f5
Jian Lee <ramosjianlee@gmail.com>
2025-09-26 20:55:45 +0800

    initialize app with Express and CORS middleware

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const contactsManagerRoutes = require("./routes");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/contacts", contactsManagerRoutes);
+
+module.exports = app;
