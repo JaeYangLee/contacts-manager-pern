b6796168d839be9f602e21b8a725ac243f36b19e
Jian Lee <ramosjianlee@gmail.com>
2025-09-26 20:55:24 +0800

    implement contacts manager controller with CRUD operations

---
+++ backend/controllers/contactsManagerController.js
@@ -0,0 +1,65 @@
+const contactsManagerModel = require("../models/contactsManagerModel");
+
+const getContacts = async (req, res) => {
+  try {
+    const allContacts = await contactsManagerModel.getContacts();
+    res.json(allContacts);
+  } catch (err) {
+    console.error("Error Fetching Contacts!", err.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const addContact = async (req, res) => {
+  try {
+    const { name, number, email } = req.body;
+    const newContact = await contactsManagerModel.addContact(
+      name,
+      number,
+      email
+    );
+    res.json(newContact);
+  } catch (err) {
+    console.error("Error Adding Contact!", err.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const updateContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, number, email } = req.body;
+    const updatedContact = await contactsManagerModel.updateContact(
+      id,
+      name,
+      number,
+      email
+    );
+    res.json(updatedContact);
+  } catch (err) {
+    console.error("Error Updating Contact!", err.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedContact = await contactsManagerModel.deleteContact(id);
+    if (!deletedContact) {
+      return res.status(400).json({ message: "Contact Not Found!" });
+    } else {
+      return res.status(200).json({ message: "Contact Deleted..." });
+    }
+  } catch (err) {
+    console.error("Error Deleting Contact!", err.message);
+    res.status(500).json({ error: "Server Error" });
+  }
+};
+
+module.exports = {
+  getContacts,
+  addContact,
+  updateContact,
+  deleteContact,
+};
